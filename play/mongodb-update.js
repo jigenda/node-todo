95df4dbf2da84614a551087d3efc7bd197e7b54f
RonaldTuibeo <ronald.tuibeo@gmail.com>
2018-05-07 22:38:28 +1000

    Added update script

---
+++ play/mongodb-update.js
@@ -0,0 +1,61 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+ 
+const dbName = 'TodoApp';
+
+ 
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+
+    }
+     
+
+    // client.db(dbName).collection('Todos').deleteMany({text:'Learn C'}).then((result) => {
+    //     console.log( result);
+        
+    // });
+
+    // client.db(dbName).collection('Todos').deleteOne({text:'Learn C'}).then((result) => {
+    //     console.log( result);
+        
+    // });
+
+//     client.db(dbName).collection('Todos').findOneAndUpdate({
+//         _id: new ObjectID('5af01c6513fa9b5dda9d8f64'
+    
+//     )},{
+//         $set: { completed: true}
+//     },{
+//        returnOriginal: false 
+//     }
+// ).then((result) => {
+//         console.log( result);
+        
+//     });
+
+client.db(dbName).collection('Users').findOneAndUpdate({
+    name: 'Josh'
+},{
+    $set: {
+        name: 'Jing'
+    },
+    $inc: {
+        age: 1
+    }
+},{
+    returnOriginal:false
+}).then((result) => {
+    console.log(result);
+});
+
+   
+    //console.log(db);
+     
+     
+
+     
+
+    client.close();
+});
